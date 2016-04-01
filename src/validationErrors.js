7e2577193dce1abeaa1e1c060552a1fdcd069bb6
Adam Royle <adam@bigfish.tv>
2016-04-01 17:13:35 +1000

    Fix typo.

--- src/validationErrors.js
+++ src/validationErrors.js
@@ -3,7 +3,7 @@ import map from 'lodash/map'
 
 /**
  * Returns a flat array of error objects suitable for
- * `react-forms` externalErrroList parameter, from a deep
+ * `react-forms` externalErrorList parameter, from a deep
  * nested object that matches CakePHP 3 validation errors.
  * 
  * @param   {Object}  errors
